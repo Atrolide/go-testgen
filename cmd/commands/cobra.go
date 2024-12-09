8059d9d80f7f87cf36806b2567c2bb3e9ac4c6ad
Atrolide <loszewskigrzegorz@gmail.com>
2024-12-09 21:33:17 +0100

    update: color for error message

--- cmd/commands/cobra.go
+++ cmd/commands/cobra.go
@@ -2,9 +2,11 @@ package commands
 
 import (
 	"fmt"
-	"github.com/Atrolide/go-testgen/internal/misc"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/Atrolide/go-testgen/internal/misc"
+	"github.com/mitchellh/colorstring"
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
@@ -29,13 +31,26 @@ func init() {
 	// Disable the default completion command
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 
+	// Hack: Disable command suggestions for unknown commands
+	rootCmd.DisableSuggestions = true
+
+	// Silence usage and errors
+	rootCmd.SilenceUsage = true
+	rootCmd.SilenceErrors = true
+
 	// Add the --version flag to the root command
 	rootCmd.Flags().BoolP("version", "v", false, "Print the version of testgen")
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		// Output error
+		// Todo: Customize error message, kick out to another file, call a command here
+		fmt.Println(colorstring.Color(fmt.Sprintf("\n[bold][red]Error: %s\n", err.Error())))
+
+		// Output help
+		// Todo: Output help for command on which the error occured
+		rootCmd.Help()
 		os.Exit(1)
 	}
 }
