fac1fa2b5ce518f674ba70dab2dfdacf397b207b
Atrolide <loszewskigrzegorz@gmail.com>
2024-12-21 22:07:37 +0100

    feature: Added a --file flag for the generate command, better handling

--- cmd/commands/generate.go
+++ cmd/commands/generate.go
@@ -6,13 +6,24 @@ import (
 )
 
 var generateCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "Generate a file",
+	Use:     "generate",
+	Aliases: []string{"g", "gen"},
+	Short:   "Generate sample test file(s)",
+	Args:    cobra.NoArgs,
+	Example: "testgen generate --file <file_path>",
 	Run: func(cmd *cobra.Command, args []string) {
-		generator.FileGen()
+		fileFlag, _ := cmd.Flags().GetString("file")
+		if fileFlag != "" {
+			// Generate a test file for a given file
+			generator.FileGenByFile(fileFlag)
+			return
+		}
 	},
 }
 
 func init() {
+	// Add the --file flag to the generate command
+	generateCmd.Flags().StringP("file", "f", "", "Generate a test for a given file")
+
 	rootCmd.AddCommand(generateCmd)
 }
--- internal/generator/filegen.go
+++ internal/generator/filegen.go
@@ -2,6 +2,7 @@ package generator
 
 import (
 	"fmt"
+	"github.com/Atrolide/go-testgen/pkg/helpers"
 	"github.com/mitchellh/colorstring"
 	"time"
 )
@@ -9,16 +10,23 @@ import (
 // TEMP: Mock method for testing purposes. Replace with real implementation.
 // FIXME: Implement a proper file generation solution.
 
-func FileGen() {
-	fmt.Printf("\nGenerating a file...\n\n")
+func FileGenByFile(fileFlag string) {
+	// Use IsFile to check if the path is a valid file
+	if !helpers.IsFile(fileFlag) {
+		errorMessage := fmt.Sprintf("\n[bold][red]Error: The '%s' either does not exist or is not a regular file.\n", fileFlag)
+		fmt.Println(colorstring.Color(errorMessage))
+		return
+	}
+
+	fmt.Printf("\nGenerating a test for the file: %s...\n\n", fileFlag)
 
 	startTime := time.Now()
-	time.Sleep(time.Duration(0.021 * float64(time.Second)))
+	time.Sleep(time.Duration(0.021 * float64(time.Second))) // Simulate file generation delay
 	elapsedTime := time.Since(startTime)
 
 	elapsedTimeMs := elapsedTime.Seconds() * 1000
 	elapsedTimeFormatted := fmt.Sprintf("%.2f ms", elapsedTimeMs)
 
-	successMessage := fmt.Sprintf("[bold][green]Test File Generated in %s\n", elapsedTimeFormatted)
+	successMessage := fmt.Sprintf("[bold][green]Test File Generated for '%s' in %s\n", fileFlag, elapsedTimeFormatted)
 	fmt.Println(colorstring.Color(successMessage))
 }
